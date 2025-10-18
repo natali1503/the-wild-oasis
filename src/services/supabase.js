171af737bb1ad38339d8f8f8d409ffe811b73b92
natali1503 <nfv1503@gmail.com>
2025-10-18 09:13:09 +0500

    fix: connecting to supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 /* eslint-disable */
-export const supabaseUrl = "https://qybsmspdkatuoaytxgbc.supabase.co";
-const supabaseKey = `${process.env.REACT_APP_API_KEY}`;
+export const supabaseUrl = "https://bonczweuqwwvvgcdimju.supabase.co";
+const supabaseKey = `${import.meta.env.VITE_REACT_APP_API_KEY}`;
 const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
     define: {
-      "process.env.REACT_APP_API_KEY": JSON.stringify(env.REACT_APP_API_KEY),
+      "process.env.VITE_API_KEY": JSON.stringify(env.VITE_REACT_APP_API_KEY),
     },
     plugins: [react(), eslint()],
     base: "/the-wild-oasis",
